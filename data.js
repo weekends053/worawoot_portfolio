7366074d5c882622123b804bd8a563ad1fe3a1a8
614259021@webmail.npru.ac.th <614259021@webmail.npru.ac.th>
2020-09-28 14:22:28 +0700

    Initial portfolio project Commit

---
+++ data.js
@@ -0,0 +1,53 @@
+const data = {
+    "fullname" : "Mr. Worawoot Bunsri",
+    "nickname" : "ZBew",
+    "motto" : "When you stop advancing, you’ve already begun to retreat.",
+    "work" : "Student at Software Engineering Program in Nakhon Pathom Rajabhat University",
+    "aboutme" : {
+        "name" : "Mr.Worawoot Bunsri",
+        "nickname" : "ZBew",
+        "sex" : "Male",
+        "education" : "Songphinongwittaya School",
+        "date" : "Sunday 25th July 1997",
+        "age" : "24 years",
+        "nationality" : "Thai",
+        "race" : "Thai",
+        "religion" : "Buddhism",
+        "bloodgroup" : "O",
+        "address" : "323/332 Moo.1 Mueang Borphub  NakhonPathome 73000",
+        "phone" : "0955896970",
+        "email" : "614259053@webmail.npru.ac.th",
+        "characteristic" : "Talk openly",
+        "hobbies" : "Listen to music, See movie, Betta fish farming, Learn more about your interests.",
+        "sport" : "Futsal",
+        "pets" : "dog",
+        "attitude" : "Positive",
+        "motto" : "When you stop advancing, you’ve already begun to retreat."
+    },
+    "education" : {
+        "education1" : "kabure School",
+        "education2" : "kabure School",
+        "education3" : "kabure School",
+        "education4" : "Nakhon Pathom Rajabhat University"
+    },
+    "experience" : {
+        "experience1" : "none"
+    },
+    "skill" : {
+        "html" : "Level-Basic",
+        "css" : "Level-Basic",
+        "php" : "Level-Basic",
+        "java" : "Level-Basic",
+        "javascript" : "Level-Basic",
+        "sql" : "Level-Basic",
+        "json" : "Level-Basic"
+    },
+    "contact" : {
+        "fb" : "สะ' บิว",
+        "ig" : "im.billyz",
+        "line" : "bew_wrp",
+        "email" : "614259053@webmail.npru.ac.th",
+        "phone" : "0955896970"
+    }
+};
+module.exports = data;
